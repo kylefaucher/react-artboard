035323b49fd343c77b25fb71edc5c9a4dbc86e1c
viol5229 <43625033+viol5229@users.noreply.github.com>
2019-12-04 22:44:02 -0700

    buttons for controls added

--- src/Controls.js
+++ src/Controls.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react';
 
-class Controls extends Component{
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-
-    return(
-      <div>
-
-      Controls go here
-
-      </div>
-      );
-  }
+class Controls extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    /*can throw an img tag inside of the button tag(for icons)*/
+    render(){
+        return(
+                <div className="control">
+                    <li className="control-item"><button>arrow</button></li>
+                    <li className="control-item"><button>square</button></li>
+                    <li className="control-item"><button>triangle</button></li>
+                    <li className="control-item"><button>circle</button></li>
+                    <li className="control-item"><button>line</button></li>
+                    <li className="control-item"><button>pen</button></li>
+                    <li className="control-item"><button>text</button></li>
+                </div>
+        );
+    }
 }
 
 export default Controls;
