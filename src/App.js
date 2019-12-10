e43fde4d0af372015f95e1a7da804a9b042c3a94
viol5229 <43625033+viol5229@users.noreply.github.com>
2019-12-10 10:11:47 -0700

    able to add shapes

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import Controls from './Controls.js';
 import Artboard from './Artboard.js';
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      currentColor: '',
-      currentTool: ''
-    };
+    constructor(props){
+        super(props);
+        this.itemID = 0;
+        this.state = {
+            currentColor: '',
+            currentTool: '',
+            shapes: [
+                {shape: '', color: '', positionX: '', positionY: '', id: ''}
+            ]
+        };
   }
+      
+    addItem = (event, shape) => {
+        this.itemID = this.itemID + 1;
+        const newShape = Object.assign([], this.state.shapes)
+        newShape.push({
+            shape: this.state.shape,
+            color: 'blue',
+            positionX: 0,
+            positionY: 0,
+            id: this.itemID
+        })
+        this.setState(
+            {shapes:newShape}
+        )
+    }
 
   render(){
 
     return(
       <div>
         <h1>ART BOARD</h1>
-        <Controls />
-        <Artboard />
-
+        <Controls addItem={this.addItem}/>
+        <Artboard shapes={this.state.shapes}/>
       </div>
       );
   }
