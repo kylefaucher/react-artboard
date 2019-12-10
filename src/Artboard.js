33afbd6c6f7f1a6d85718bf8e94c7e527bc7eb6f
choybm <32180312+choybm@users.noreply.github.com>
2019-12-10 09:29:55 -0700

    move and resize elements

--- src/Artboard.js
+++ src/Artboard.js
@@ -1,34 +1,74 @@
 import React, {Component} from 'react';
-import {Layer, Rect, Stage} from 'react-konva';
+import { render } from 'react-dom';
+import {Layer, Rect, Stage, Transformer} from 'react-konva';
+import Rectangle from "./Rectangle";
 
-class Artboard extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-    	shapes: [
-    		{shape: 'rectangle', color: 'blue', positionX: '0', positionY: '0'}
-    	]
-    };
-  }
+const storeRectangles = [
+  // {
+  //   x: 10,
+  //   y: 10,
+  //   width: 100,
+  //   height: 100,
+  //   fill: 'red',
+  //   id: 'rect1'
+  // },
+  // {
+  //   x: 150,
+  //   y: 150,
+  //   width: 100,
+  //   height: 100,
+  //   fill: 'green',
+  //   id: 'rect2'
+  // }
+];
 
-  render(){
+function Artboard() {
+
+  const [rectangles, setRectangles] = React.useState([storeRectangles]);
+  const [selectedId, selectShape] = React.useState(null);
+  
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //   	shapes: [
+  //   		{shape: 'rectangle', color: 'blue', positionX: '0', positionY: '0'}
+  //   	]
+  //   };
+  // }
 
     return(
       <div>
 
-	      <Stage className = "stage" width={700} height={700}>
+	      <Stage className = "stage" width={700} height={700} onMouseDown={e => {
+        // deselect when clicked on empty area
+        const clickedOnEmpty = e.target === e.target.getStage();
+        if (clickedOnEmpty) {
+          selectShape(null);
+        }
+      }}>
 	        <Layer>
-	            <Rect
-	                x={10} y={10} width={50} height={50}
-	                fill={'blue'}
-	                shadowBlur={10}
-	            />
-	        </Layer>
+        {rectangles.map((rect, i) => {
+          return (
+            <Rectangle
+              key={i}
+              shapeProps={rect}
+              isSelected={rect.id === selectedId}
+              onSelect={() => {
+                selectShape(rect.id);
+              }}
+              onChange={newAttrs => {
+                const rects = rectangles.slice();
+                rects[i] = newAttrs;
+                setRectangles(rects);
+              }}
+            />
+          );
+        })}
+      </Layer>
 	      </Stage>
 
       </div>
       );
   }
-}
 
 export default Artboard;
